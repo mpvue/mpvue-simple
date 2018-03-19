688e8b256793212a5072dd5cc5043dfa786172f0
aorz <rememberwz@gmail.com>
2018-03-19 20:08:05 +0800

    update：check version

--- utils/index.js
+++ utils/index.js
@@ -1,18 +1,26 @@
 const path = require('path')
-const childProcess = require('child_process')
-const { execSync } = childProcess
-
+const http = require('http');
 // upgrade logger
 function upgradeLogger () {
   const currPkg = require('../package.json')
   const cwd = path.resolve(__dirname, '../')
   const pkgName = currPkg.name || 'mpvue-simple'
-  const pkg = JSON.parse(execSync(`curl \`npm config get registry\`${pkgName} -s`, { cwd }))
-  const latestVesion = pkg['dist-tags'].latest
 
-  if (latestVesion !== currPkg.version) {
-    console.log(`${pkgName} 有新版本 ${latestVesion} 啦，请注意升级。\n例如: npm install ${pkgName} -g\n`)
-  }
+  http.get('http://registry.npmjs.org/' + currPkg.name, (res) => {
+    let rawData = '';
+    res.on('data', (chunk) => rawData += chunk);
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        let latestVesion = parsedData['dist-tags'].latest;
+        if (latestVesion !== currPkg.version) {
+          console.log(`${pkgName} 有新版本 ${latestVesion} 啦，请注意升级。\n例如: npm install ${pkgName} -g\n`)
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    });
+  })
 }
 
 exports.upgradeLogger = upgradeLogger
